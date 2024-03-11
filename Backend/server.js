3056977cb92a641fe4703b3b4156b4f5f73d74aa
Shruti-Bansal-Gupta <shrutidev.bansal@gmail.com>
2024-03-11 23:53:45 +0530

    adding get request

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 app.use(express.json()); // Middleware to parse JSON bodies
 
+app.get("/books", (req, res) => {
+    res.json(courses);
+  });
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
