8cca11cb147b9f3021f559ec833ea21558d6a665
Shruti-Bansal-Gupta <shrutidev.bansal@gmail.com>
2024-04-16 22:58:28 +0530

    installed the required dependencies

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 5000;
+const cors = require('cors')
+
+//middleware
+app.use(cors());
 app.use(express.json()); // Middleware to parse JSON bodies
 
 app.get("/books", (req, res) => {
     res.json(courses);
   });
 
-const PORT = 3001;
+app.get('/',(req,res)=>{
+  res.send('hello world')
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
