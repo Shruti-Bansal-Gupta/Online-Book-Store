8d1402c169e72c8596b738a36e671e3fc99f60a7
Shruti-Bansal-Gupta <shrutidev.bansal@gmail.com>
2024-04-16 23:41:48 +0530

    made the mongodb connection

--- Backend/server.js
+++ Backend/server.js
@@ -15,6 +15,46 @@ app.get('/',(req,res)=>{
   res.send('hello world')
 })
 
+//mongodb configuration 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mern-book-store:lQEOBTvrYiudMJuC@cluster0.vlsqgd5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    //create a connection of documents
+    const booksCollection = client.db("booksInventory").collection("books");
+
+    //insert a book to the database: post method
+    app.post("/upload-book",async(req,res)=>{
+       const data = req.body;
+       const result = await booksCollection.insertOne(data);
+       res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
