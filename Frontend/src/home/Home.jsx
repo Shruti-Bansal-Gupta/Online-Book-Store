2a9e846999904e1f9cedb242025aea5ae7db317b
Shruti-Bansal-Gupta <shrutidev.bansal@gmail.com>
2024-04-19 23:27:18 +0530

    Basic components created

---
+++ Frontend/src/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className=''>
+        <div className='h-screen'>
+           Home
+        </div>
+        <div className='h-screen bg-red-600'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Home
